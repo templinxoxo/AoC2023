981b8491b698845955803070a5e08813b9c39493
Michal Templin <michaltemplin@surferseo.com>
2023-11-30 17:50:21 +0100

    chore: initial setup
    added:
    * initial elixir setup
    * api call module
    * local scripts and env (not in repo)

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aoc
+
+Advent of code '23 solutions in elixir.
+
+https://adventofcode.com/2023
---
+++ lib/api.ex
@@ -0,0 +1,19 @@
+defmodule Api do
+  use Tesla
+  use Memoize
+
+  plug(Tesla.Middleware.BaseUrl, "https://adventofcode.com/2023/day/")
+
+  plug(Tesla.Middleware.Headers, [
+    {"cookie", "session=#{session_cookie()}}"}
+  ])
+
+  def session_cookie do
+    System.get_env("AOC_SESSION_COOKIE")
+  end
+
+  defmemo get_input(day) do
+    {:ok, response} = get("#{day}/input")
+    response.body
+  end
+end
