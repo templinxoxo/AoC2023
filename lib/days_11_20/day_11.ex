f4c94c5c9059390cd921babc8c97bcffdf4a6634
Michal Templin <michaltemplin@surferseo.com>
2023-12-11 09:55:29 +0100

    day11: part 1

---
+++ lib/days_11_20/day_11.ex
@@ -0,0 +1,78 @@
+defmodule Day11 do
+  # execute methods
+
+  def execute_part_1(data \\ fetch_data()) do
+    data
+    |> parse_input()
+    |> find_galaxies()
+    |> expand_universe()
+    |> calculate_galaxy_distances_sum()
+  end
+
+  def find_galaxies(galaxy_map) do
+    galaxy_map
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {row, y_index} ->
+      row
+      |> Enum.with_index()
+      |> Enum.map(fn
+        {".", _} ->
+          nil
+
+        {"#", x_index} ->
+          {x_index, y_index}
+      end)
+    end)
+    |> Enum.reject(&is_nil/1)
+  end
+
+  def expand_universe(galaxy_list) do
+    empty_x_axis = get_empty_axis(Enum.map(galaxy_list, fn {x, _y} -> x end))
+    empty_y_axis = get_empty_axis(Enum.map(galaxy_list, fn {_x, y} -> y end))
+
+    galaxy_list
+    |> Enum.map(fn {x, y} ->
+      expansion_x = empty_x_axis |> Enum.filter(&(&1 < x)) |> length()
+      expansion_y = empty_y_axis |> Enum.filter(&(&1 < y)) |> length()
+
+      {x + expansion_x, y + expansion_y}
+    end)
+  end
+
+  def calculate_galaxy_distances_sum(galaxy_list) do
+    0..(length(galaxy_list) - 2)
+    |> Enum.map(&Enum.slice(galaxy_list, &1, length(galaxy_list)))
+    |> Enum.map(fn [{x0, y0} | galaxies] ->
+      Enum.map(galaxies, fn {x, y} ->
+        abs(x0 - x) + abs(y0 - y)
+      end)
+      |> Enum.sum()
+    end)
+    |> Enum.sum()
+  end
+
+  def get_empty_axis(galaxies_single_axis) do
+    {min_val, max_val} = Enum.min_max(galaxies_single_axis)
+
+    min_val..max_val
+    |> Enum.reject(&(&1 in galaxies_single_axis))
+  end
+
+  # helpers
+  def fetch_data() do
+    Api.get_input(11)
+  end
+
+  def parse_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(fn row ->
+      row
+      |> String.trim()
+      |> String.split("")
+      |> Enum.reject(&(&1 == ""))
+    end)
+  end
+end
---
+++ test/days_11_20/day_11.exs
@@ -0,0 +1,24 @@
+defmodule Day11Test do
+  use ExUnit.Case
+
+  alias Day11, as: Subject
+
+  @test_data """
+  ...#......
+  .......#..
+  #.........
+  ..........
+  ......#...
+  .#........
+  .........#
+  ..........
+  .......#..
+  #...#.....
+  """
+
+  test "execute_part_1/1" do
+    result = Subject.execute_part_1(@test_data)
+
+    assert result == 374
+  end
+end
