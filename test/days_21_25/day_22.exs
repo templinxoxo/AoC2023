80de297e4a745d6398f7fded09891fb231bea0f9
Michal Templin <michaltemplin@surferseo.com>
2023-12-22 23:14:16 +0100

    day22: part - add tricky inputs, still not working

--- test/days_21_25/day_22.exs
+++ test/days_21_25/day_22.exs
@@ -13,9 +13,69 @@ defmodule Day22Test do
   1,1,8~1,1,9
   """
 
-  test "execute_part_1/1" do
-    result = Subject.execute_part_1(@test_data)
+  describe "execute_part_1/1" do
+    test "test data" do
+      result = Subject.execute_part_1(@test_data)
 
-    assert result == 5
+      assert result == 5
+    end
+
+    test "modified test data" do
+      result =
+        (@test_data <> "\n1,1,10~1,1,10")
+        |> Subject.execute_part_1()
+
+      assert result == 5
+    end
+
+    test "edge case 1" do
+      result =
+        """
+        0,0,1~0,1,1
+        1,1,1~1,1,1
+        0,0,2~0,0,2
+        0,1,2~1,1,2
+        """
+        |> Subject.execute_part_1()
+
+      assert result == 3
+    end
+
+    test "edge case 2" do
+      result =
+        """
+        0,0,1~1,0,1
+        0,1,1~0,1,2
+        0,0,5~0,0,5
+        0,0,4~0,1,4
+        """
+        |> Subject.execute_part_1()
+
+      assert result == 2
+    end
+
+    test "edge case 3" do
+      result =
+        """
+        0,0,1~2,0,1
+        0,2,2~2,2,2
+        1,0,4~1,2,4
+        """
+        |> Subject.execute_part_1()
+
+      assert result == 3
+    end
+
+    test "edge case 4" do
+      result =
+        """
+        0,2,3~0,2,4
+        0,0,1~0,0,2
+        0,0,5~0,2,6
+        """
+        |> Subject.execute_part_1()
+
+      assert result == 3
+    end
   end
 end
